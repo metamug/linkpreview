da364792d4d7abb869d1d005e472eb53a2957316
Deepak Mishra <deepak@metamug.com>
2014-06-21 16:11:55 +0530

    Updated Product Strategy Test

--- src/test/java/net/metamug/metascrapper/entity/MetaDataFactoryTest.java
+++ src/test/java/net/metamug/metascrapper/entity/MetaDataFactoryTest.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package net.metamug.metascrapper.entity;
 
-import net.metamug.metascrapper.util.MetaExtract;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,22 +18,22 @@ import org.junit.Test;
  * @author deepak
  */
 public class MetaDataFactoryTest {
-    
+
     public MetaDataFactoryTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -41,38 +41,44 @@ public class MetaDataFactoryTest {
     /**
      * Test of create method, of class MetaDataFactory.
      */
-
 //    @Test
     public void testArticle() {
 //        System.out.println("create");
         String url = "http://stackoverflow.com/questions/5963269/how-to-make-a-great-r-reproducible-example";
-        
+
         WebMetaData result = MetaDataFactory.create(url);
 //        System.out.println(result.getDescription());       
 //        System.out.println(result.getThumbnail().getUrl());
     }
-    
+
     @Test
-    public void testProduct()
-    {
-//        String url = "http://www.flipkart.com/moto-e/p/itmdvuwsybgnbtha?pid=MOBDVHC6XKKPZ3GZ&otracker=hp_mod_electronics_bestseller_prd_img";
-//        String url = "http://www.flipkart.com/htc-desire-210/p/itmdvzg8bnph9xja?pid=MOBDVZ6TDYZCKRDC&icmpid=reco_pp_same_mobile_1&ppid=MOBDVHC6XKKPZ3GZ";
+    public void testProduct() {
 
-        String url = "http://www.amazon.in/gp/product/0984756302/ref=s9_simh_gw_p14_d1_i3?pf_rd_m=A1VBAL9TL5WCBF&pf_rd_s=center-2&pf_rd_r=1365DR4RT60MNYXBNC97&pf_rd_t=101&pf_rd_p=402519107&pf_rd_i=1320006031";
+        /*The following test data will be generated from an external file in future*/
+        List<String> testData = new ArrayList<>();
+        testData.add("http://www.flipkart.com/moto-e/p/itmdvuwsybgnbtha?pid=MOBDVHC6XKKPZ3GZ&otracker=hp_mod_electronics_bestseller_prd_img");
+        testData.add("http://www.flipkart.com/htc-desire-210/p/itmdvzg8bnph9xja?pid=MOBDVZ6TDYZCKRDC&icmpid=reco_pp_same_mobile_1&ppid=MOBDVHC6XKKPZ3GZ");
+        testData.add("http://www.amazon.in/gp/product/0984756302/ref=s9_simh_gw_p14_d1_i3?pf_rd_m=A1VBAL9TL5WCBF&pf_rd_s=center-2&pf_rd_r=1365DR4RT60MNYXBNC97&pf_rd_t=101&pf_rd_p=402519107&pf_rd_i=1320006031");
 
-        ProductMetaData p = (ProductMetaData) MetaDataFactory.create(url);
-        
-        System.out.println(p.getName());
-        System.out.println(p.getPrice());
-        System.out.println(p.getPriceCurrency());
-        System.out.println(p.getRatingCount());
-        System.out.println(p.getRatingValue());
-        System.out.println(p.getReviewCount());
+        for (String url : testData) {
+            ProductMetaData p = (ProductMetaData) MetaDataFactory.create(url);
+            System.out.println("===== Product Metadata =====");
+            System.out.println("Product Name: " + p.getName());
+            System.out.println("Product Price: " + p.getPrice());
+            System.out.println("Product Price Currency: " + p.getPriceCurrency());
+            System.out.println("Product Rating Count: " + p.getRatingCount());
+            System.out.print("Product Rating: ");
+            //To print stars. "\u00BD" for printing half stars :)
+            for (int i = 0; i < p.getRatingValue(); i++) {
+                System.out.print("\u2605");
+            }
+            System.out.println("(" + p.getRatingValue() + ")");
+            System.out.println("Product Review Count: " + p.getReviewCount());
+        }
     }
-    
+
 //    @Test
-    public void testImage()
-    {
+    public void testImage() {
         String url = "https://www.flickr.com/photos/spoutnik53/14454170711/in/explore-2014-06-19";
         ImageMetaData i = (ImageMetaData) MetaDataFactory.create(url);
 
@@ -80,5 +86,5 @@ public class MetaDataFactoryTest {
         System.out.println(i.getAuthor());
 //        System.out.println(i.getViews());
     }
-    
+
 }
