3b9fb5e8818fec9afb17bca05eb13170220b5dc2
Deepak Mishra <deepak.mishra@metamug.com>
2017-07-31 22:33:48 +0530

    exception handling for metamug request processing

--- src/main/java/net/metamug/scrapper/exec/SnippetGenerator.java
+++ src/main/java/net/metamug/scrapper/exec/SnippetGenerator.java
@@ -10,9 +10,16 @@ public class SnippetGenerator implements RequestProcessable {
 
     public Object process(Map<String, String> param, DataSource ds,
         Map<String, String> requestHeaders){
-		String url = "http://stackoverflow.com/questions/5963269/how-to-make-a-great-r-reproducible-example";
-        WebMetaData result = MetaDataFactory.create(url);
-
+		String url = param.get("url");
+        WebMetaData result = null;
+        try{
+        	result = MetaDataFactory.create(url);
+        }catch(Exception e){
+        	result = new WebMetaData("Unable to read information",
+        	 "", "", "Something is wrong with this webpage or we aren't good enough yet.", 
+        	 "https://lh3.googleusercontent.com/-MVEG-8gyeEs/WX9f1ZSbspI/AAAAAAAACi0/muowN-vTfGsEfAjK1LvrETXIa0nz6OnUwCL0BGAYYCw/h215/2017-07-31.png");
+        	return result;
+        }
         return result;
 	}
 }
