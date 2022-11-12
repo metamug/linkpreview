d12d0f88583e7bfc0adefb6b14e440e244c33b3d
Deepak Mishra <d3ep4k@users.noreply.github.com>
2022-11-12 19:50:47 +0530

    Update ImageUtil.java

--- src/main/java/net/metamug/scrapper/util/ImageUtil.java
+++ src/main/java/net/metamug/scrapper/util/ImageUtil.java
@@ -1,18 +1,3 @@
-/**
- * ***********************************************************************
- *
- * METAMUG.COM CONFIDENTIAL __________________
- *
- * [2013] - [2014] metamug.com All Rights Reserved.
- *
- * NOTICE: All information contained herein is, and remains the property of
- * metamug.com and its suppliers, if any. The intellectual and technical
- * concepts contained herein are proprietary to metamug.com and its suppliers
- * and may be covered by Indian and Foreign Patents, patents in process, and are
- * protected by trade secret or copyright law. Dissemination of this information
- * or reproduction of this material is strictly forbidden unless prior written
- * permission is obtained from metamug.com
- */
 package net.metamug.scrapper.util;
 
 import java.io.IOException;
